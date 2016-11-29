cbe358523dc74bb3684f9b8f10b52bf6a8cdca79
shazwan <shazwanx23@gmail.com>
2016-11-29 18:57:29 +0800

    organizing test blocks

--- server/server.test.js
+++ server/server.test.js
@@ -3,29 +3,37 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-	request(app)
-		.get('/')
-		.expect(404)
-		.expect((res) => {
-			expect(res.body).toInclude({
-				error: 'Page not found.'
+describe('Server tests', () => {
+
+		describe('GET /', () => {
+			it('should return hello world response', (done) => {
+				request(app)
+					.get('/')
+					.expect(404)
+					.expect((res) => {
+						expect(res.body).toInclude({
+							error: 'Page not found.'
+						});
+					})
+					.end(done);
 			});
 		})
-		.end(done);
-});
+
+		describe('GET /users', () => {
+			it('should return an array of users', (done) => {
+				request(app)
+					.get('/users')
+					.expect(200)
+					.expect((res) => {
+						expect(res.body).toInclude({
+							name: 'shazwan',
+							age: 21
+						}).toBeA('object');
+					})
+					.end(done);
+			});
+		})
+})
 
 //assert status = 200
 //assert that you exist in users array
-it('should return an array of users', (done) => {
-	request(app)
-		.get('/users')
-		.expect(200)
-		.expect((res) => {
-			expect(res.body).toInclude({
-				name: 'shazwan',
-				age: 21
-			}).toBeA('object');
-		})
-		.end(done);
-});
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,43 +2,51 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () =>{
-	var res = utils.add(33, 11);
+describe('Utils', () => {
 
-	expect(res).toBe(44).toBeA('number');
-});
+	describe('#add' , () => {
+		it('should add two numbers', () =>{
+			var res = utils.add(33, 11);
 
-it('should asyncAdd two numbers', (done) => {
-	utils.asyncAdd(4,3, (sum) => {
-		expect(sum).toBe(7).toBeA('number');
-		done();
+			expect(res).toBe(44).toBeA('number');
+		});
+	});
+
+
+
+	it('should asyncAdd two numbers', (done) => {
+		utils.asyncAdd(4,3, (sum) => {
+			expect(sum).toBe(7).toBeA('number');
+			done();
+		})
 	})
-})
 
-it('should square a number', () => {
-	var res = utils.square(4);
+	it('should square a number', () => {
+		var res = utils.square(4);
 
-	expect(res).toBe(16).toBeA('number');
-});
+		expect(res).toBe(16).toBeA('number');
+	});
 
-it('should async square a number', (done) => {
-	utils.asyncSquare(3, (squares) => {
-		expect(squares).toBe(9).toBeA('number');
-		done();
+	it('should async square a number', (done) => {
+		utils.asyncSquare(3, (squares) => {
+			expect(squares).toBe(9).toBeA('number');
+			done();
+		})
 	})
-})
 
-it('should verify first and last names are set', () => {
-	var user = {
-		age: 21,
-		location: 'my location'
-	};
-	utils.setName(user, 'shazwan shah');
-	expect(user).toInclude({
-		first_name: 'shazwan',
-		last_name: 'shah'
-	}).toBeA('object');
-})
+	it('should verify first and last names are set', () => {
+		var user = {
+			age: 21,
+			location: 'my location'
+		};
+		utils.setName(user, 'shazwan shah');
+		expect(user).toInclude({
+			first_name: 'shazwan',
+			last_name: 'shah'
+		}).toBeA('object');
+	})
+
+});
 
 // it('should expect some values', () => {
 // 	// expect(12).toNotBe(11);
